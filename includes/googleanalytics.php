007632c1570afb03515993574d42a5c9a3b5f87f
Adam Jenkins <jenkinsac@health.missouri.edu>
2012-12-28 14:34:16 -0600

    Added Google Analytics

---
+++ includes/googleanalytics.php
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+	
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-31341113-3']);
+	_gaq.push(['_trackPageview']);
+	
+	// User information
+	_gaq.push(['_setCustomVar', 1, 'User ID', <?php echo $_SESSION['user_id']; ?>, 2]);
+	_gaq.push(['_setCustomVar', 2, 'Department ID', <?php echo $_SESSION['department']; ?>, 2]);
+	
+	// Pages Data
+	//_gaq.push(['_trackEvent', 'Media', 'Uploaded', 'This is the media title.']);
+	
+	// Users Data
+	
+	
+	// Media Data
+	
+	
+	// News Data
+	
+	
+	// Help Data
+	
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	
+</script>
