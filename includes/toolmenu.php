2f0f957a48cb379064ce8de9c3cdd47d27728bd6
Adam Jenkins <adamcole83@gmail.com>
2012-12-28 15:11:44 -0600

    Added git branch to toolbar. Active when not in 'production' ENVIRONMENT

--- includes/toolmenu.php
+++ includes/toolmenu.php
@@ -1,5 +1,6 @@
 		<div id="toolmenu" <?php echo defined('ENVIRONMENT') && ENVIRONMENT == 'maintenance' ? 'class="maintenance"':null; ?>>
 			<div class="container">
+				
 				<?php if(defined('ENVIRONMENT') && ENVIRONMENT != 'production'): ?>
 				<script type="text/javascript">
 				$(function() {
@@ -7,6 +8,16 @@
 				});
 				</script>
 				<div class="floatLeft pulse"><?php echo strtoupper(ENVIRONMENT); ?> MODE ACTIVE</div>
+				
+				<?php
+				$stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
+				$stringfromfile = $stringfromfile[0]; //get the string from the array
+				$explodedstring = explode("/", $stringfromfile); //seperate out by the "/" in the string
+				$branchname = $explodedstring[2]; //get the one that is always the branch name
+				?>
+				
+				BRANCH: <?php echo $branchname; ?>
+				
 				<?php endif; ?>
 <!-- 				<span onclick="ShowTools('toolMessages')"><span class="msgcount">0</span> Messages</span> -->
 				<span onclick="ShowTools('toolDepartment')">Change Department</span>
