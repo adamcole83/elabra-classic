2e680f4b8b15730ea2e0708061b637c520801ae8
Adam Jenkins <adamcole83@gmail.com>
2013-03-20 13:54:28 -0500

    Moved config file and fixed ignore

--- includes/classes/phpmysqlconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER", "161.130.114.123"); // 161.130.114.123
-defined('DB_USER')   ? null : define("DB_USER", "appconnect");
-defined('DB_PASS')   ? null : define("DB_PASS", "QTzz7TytquC8Z9t");
-defined('DB_NAME')   ? null : define("DB_NAME", "cms");
