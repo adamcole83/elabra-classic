5aab81ac53daf38caf769dbe24f1a8d845897f03
Adam Jenkins <adamcole83@gmail.com>
2013-03-20 11:42:10 -0500

    Updated SQL auth

--- includes/classes/phpmysqlconfig.php
+++ includes/classes/phpmysqlconfig.php
@@ -2,6 +2,6 @@
 
 // Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "161.130.114.123"); // 161.130.114.123
-defined('DB_USER')   ? null : define("DB_USER", "ajenkins"); // ajenkins
-defined('DB_PASS')   ? null : define("DB_PASS", "d3st1Ny7"); // d3st1Ny7
+defined('DB_USER')   ? null : define("DB_USER", "appconnect");
+defined('DB_PASS')   ? null : define("DB_PASS", "QTzz7TytquC8Z9t");
 defined('DB_NAME')   ? null : define("DB_NAME", "cms");
