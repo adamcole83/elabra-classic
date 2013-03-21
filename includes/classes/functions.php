2b34672931e7d5603e7acda4e7669d66ed4b57fb
Adam Jenkins <adamcole83@gmail.com>
2013-03-21 08:40:35 -0500

    Revert "Merge branch 'master' of umhc-stgprodl.umh.edu:/var/www/html/medicine.missouri.edu/admin"
    This reverts commit 8bc87af75e7d4bb5b4f62be88ee89b9fd68049da.

--- includes/classes/functions.php
+++ includes/classes/functions.php
@@ -38,10 +38,7 @@ function get_parent($parent)
 
 function sc_branch()
 {
-	$stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
-	$stringfromfile = $stringfromfile[0]; //get the string from the array
-	$explodedstring = explode("/", $stringfromfile); //seperate out by the "/" in the string
-	return $explodedstring[2]; //get the one that is always the branch name
+	return "git branch: " . exec("git rev-parse --abbrev-ref HEAD");
 }
 
 function increment_string($str, $separator = '_', $first = 1)
@@ -185,13 +182,14 @@ function check_environment()
 
 function get_page_info($part) {
 	// set action
-	$action = ($_GET['action']) ? $_GET['action'] : 'select';
+	$action = (isset( $_GET['action'] )) ? $_GET['action'] : 'select';
+	$id = (isset( $_GET['id'] )) ? $_GET['id'] : null;
 	// set current view
 	$gCurrentView = str_replace('.php','',basename($_SERVER['PHP_SELF']));
 	if( $action )
 		$gCurrentView .= '.'.$action;
-	if( $_GET['id'] )
-		$gCurrentView .= '.'.$_GET['id'];
+	if(isset( $id ))
+		$gCurrentView .= '.'.$id;
 	
 	switch($part) {
 		case 'action':
