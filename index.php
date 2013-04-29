8bf85451830253cba8ee81bb9321216fe3e8a017
Adam Jenkins <adamcole83@gmail.com>
2013-04-29 14:22:09 -0500

    Trying to push to remote

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	$posts = new Content();
 	$posts->department = $_SESSION['department'];
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="https://www.w3.org/1999/xhtml">
 	<head>
