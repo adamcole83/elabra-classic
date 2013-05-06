b8cd6649efd767e4e97367349225addf4c23e330
Adam Jenkins <adamcole83@gmail.com>
2013-05-06 14:36:10 -0500

    Added FreshDesk Support tab

--- includes/head.php
+++ includes/head.php
@@ -12,3 +12,11 @@
 			window.previousPage = '<?php echo $_SERVER['HTTP_REFERER']; ?>';
 		</script>
 		<script type="text/javascript" src="js/jsload.php"></script>
+
+		<script type="text/javascript" src="http://assets.freshdesk.com/widget/freshwidget.js"></script>
+		<style type="text/css" media="screen, projection">
+			@import url(http://assets.freshdesk.com/widget/freshwidget.css); 
+		</style> 
+		<script type="text/javascript">
+			FreshWidget.init("", {"queryString": "", "buttonText": "Support", "buttonColor": "black", "buttonBg": "#eeb211", "alignment": "4", "offset": "335px", "url": "http://medweb.freshdesk.com", "assetUrl": "http://assets.freshdesk.com/widget"} );
+		</script>
