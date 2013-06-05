594e6a655bdcb6d1a197394792e2f0284dad3017
Adam Jenkins <adamcole83@gmail.com>
2013-06-05 10:57:52 -0500

    Added format_phone helper

--- includes/classes/functions.php
+++ includes/classes/functions.php
@@ -27,6 +27,18 @@ function image($class, $name)
 	return '<img class="'.$class.'" alt="'.$name.'" src="images/'.$name.'" />';
 }
 
+function format_phone($phone)
+{
+	$phone = preg_replace("/[^0-9]/", "", $phone);
+
+	if(strlen($phone) == 7)
+		return preg_replace("/([0-9]{3})([0-9]{4})/", "$1-$2", $phone);
+	elseif(strlen($phone) == 10)
+		return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "($1) $2-$3", $phone);
+	else
+		return $phone;
+}
+
 function get_parent($parent)
 {
 	if( $parent == 0) {
